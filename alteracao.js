bcb18425b024a25dff468cb1b0d670f700398b67
jifsvbi <joaopedro222931@gmail.com>
2024-05-21 17:27:13 -0300

    alteração basica

---
+++ alteracao.js
@@ -0,0 +1,7 @@
+// Função para simular o ganho de gemas com um multiplicador
+function ganhejoias(quantia150) {
+ console.log("Imagine que você ganhou  " + quantia * 5 + " gemas!");
+}
+
+// Simula completar um nível e ganhar 750 joias
+ganhejoias(750);
